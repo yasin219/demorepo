4824f2e9ea612eebaf9fc10dcf785e4b2fb8bf21
yasin219 <yasin8505786@gmail.com>
2025-06-23 16:35:48 +0530

    newd

--- config/database.config.js
+++ config/database.config.js
@@ -1,6 +1,6 @@
 
 
 module.exports = {
-    url: 'mongodb://"admin":"MySecureAdmin123"@13.204.98.243:27017/your_database?authSource=admin'
+    url: 'mongodb://"admin":"MySecureAdmin123"@13.204.98.243:27017/admin?authSource=admin'
 
 }
