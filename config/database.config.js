b6122afbe512cbb2c2927053e3e4e93ca8f7252a
yasin219 <yasin8505786@gmail.com>
2025-06-23 19:07:49 +0530

    ne

--- config/database.config.js
+++ config/database.config.js
@@ -1,6 +1,8 @@
 
 
 module.exports = {
-    url: 'mongodb://"admin":"MySecureAdmin123"@13.204.98.243:27017/adminauthSource=admin'
+    // url: 'mongodb://"admin":"MySecureAdmin123"@13.204.98.243:27017/adminauthSource=admin'
+   url: "mongodb://myUserAdmin:MySecurePass123@13.204.98.243:27017/admin"
+
 
 }
