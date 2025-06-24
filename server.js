326bd73fd707963843ce5868f1aed203d38cf0e6
yasin219 <yasin8505786@gmail.com>
2025-06-24 18:45:01 +0530

    n

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.connect(dbConfig.url, {
 app.get('/', (req, res) => {
     res.json({"message": "Hello Crud Node Express"});
 });
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
